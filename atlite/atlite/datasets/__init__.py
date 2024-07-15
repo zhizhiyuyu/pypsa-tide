723bad61cb9aba85fc6f16a9e54ae5de2a38c4a0
zhizhiyuyu <zhiyuzhang72@gmail.com>
2024-07-16 01:01:15 +0800

    atlite

--- atlite/atlite/datasets/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# SPDX-FileCopyrightText: 2016 - 2023 The Atlite Authors
-#
-# SPDX-License-Identifier: MIT
-
-from atlite.datasets import era5, gebco, sarah, tide
-
-modules = {"era5": era5, "sarah": sarah, "gebco": gebco, "tide": tide}
